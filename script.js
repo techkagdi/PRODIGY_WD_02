1214c627e2005e1aec5a18c9ebb0e9303a3d774f
techkagdi <mohammedkagdi68@gmail.com>
2024-06-02 14:51:10 +0530

    Completed Design

--- script.js
+++ script.js
@@ -5,44 +5,52 @@ let second = 0;
 let hours = 0;
 let minutes = 0;
 
-document.getElementById("start").addEventListener("click",()=>{
-    if (int !== null){
-      clearInterval(int);
-    }
-    int = setInterval(settime,10);
-});
-
-function settime(){
+function settime() {
   ms += 10;
-  if(ms == 1000){
+  if (ms == 1000) {
     ms = 0;
-    second ++;
-    if(second == 60){
-        second = 0;
-        minutes ++;
-        if(minutes == 60){
-            minutes = 0;
-            hour++;
-        }
+    second++;
+    if (second == 60) {
+      second = 0;
+      minutes++;
+      if (minutes == 60) {
+        minutes = 0;
+        hour++;
+      }
     }
   }
 
-time.innerHTML = hours+":"+minutes+":"+second+":"+ms;
-
+  time.innerHTML = String(hours). padStart(2,0)+ ":" + String(minutes).padStart(2,0) + ":" + String(second).padStart(2,0) + ":" + String(ms).padStart(3,0);
+ ;
 }
-document.getElementById("reset").addEventListener("click",()=>{
-   
-      clearInterval(int);
-    ms = 0;
- second = 0;
- hours = 0;
- minutes = 0;
-   
-time.innerHTML = "00:00:00:000";
-});
-document.getElementById("pause").addEventListener("click",()=>{
-   
+document.getElementById("start").addEventListener("click", () => {
+  if (int !== null) {
     clearInterval(int);
- 
+  }
+  int = setInterval(settime, 10);
+});
+document.getElementById("reset").addEventListener("click", () => {
+  clearInterval(int);
+  ms = 0;
+  second = 0;
+  hours = 0;
+  minutes = 0;
 
+  time.innerHTML = "00:00:00:000";
+  document.getElementById("container2").innerHTML = ""
+});
+
+document.getElementById("pause").addEventListener("click", () => {
+  clearInterval(int);
+});
+document.getElementById("bookmark").addEventListener("click", () => {
+    let timers = document.getElementById("timer-section").innerHTML;
+    
+    // Create a new line break element
+    let br = document.createElement("br");
+    
+    // Append the line break and the content to container2
+    let container = document.getElementById("container2");
+    container.appendChild(br);
+    container.appendChild(document.createTextNode(timers));
 });
